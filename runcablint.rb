f2a8b0b87df52b3f1f2c8836258f4ef7ef1596e6
Rob Stradling <rob@comodo.com>
2016-11-15 15:56:30 +0000

    Catch all exceptions.

--- runcablint.rb
+++ runcablint.rb
@@ -6,14 +6,18 @@ require 'enc/trans/transdb'
 require 'certlint'
 
 def runcablint(raw)
-  m = CertLint::CABLint.lint(raw)
   m2 = []
-  m.each do |msg|
-    begin
-      m2 << msg
-    rescue Encoding::UndefinedConversionError
-      m2 << "F: Encoding::UndefinedConversionError"
+  begin
+    m = CertLint::CABLint.lint(raw)
+    m.each do |msg|
+      begin
+        m2 << msg
+      rescue Encoding::UndefinedConversionError
+        m2 << "F: Encoding::UndefinedConversionError"
+      end
     end
+  rescue => e
+    m2 << "F: #{e} Exception"
   end
   return m2
 end
