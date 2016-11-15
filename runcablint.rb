9049344d1dddb82d33c93fd0d567eb9fdfec713a
Rob Stradling <rob@comodo.com>
2016-11-15 15:56:04 +0000

    Catch Encoding::UndefinedConversionError.

--- runcablint.rb
+++ runcablint.rb
@@ -6,5 +6,14 @@ require 'enc/trans/transdb'
 require 'certlint'
 
 def runcablint(raw)
-  return CertLint::CABLint.lint(raw)
+  m = CertLint::CABLint.lint(raw)
+  m2 = []
+  m.each do |msg|
+    begin
+      m2 << msg
+    rescue Encoding::UndefinedConversionError
+      m2 << "F: Encoding::UndefinedConversionError"
+    end
+  end
+  return m2
 end
